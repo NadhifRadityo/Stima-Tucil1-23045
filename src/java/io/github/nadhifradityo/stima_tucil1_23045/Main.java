77c16a8905260437aa79f54ad89d964d8956fc36
Nadhif Radityo <nadhifradityo@gmail.com>
2025-02-18 01:36:38 +0700

    feat: implement bit field optimization strategy

--- src/java/io/github/nadhifradityo/stima_tucil1_23045/Main.java
+++ src/java/io/github/nadhifradityo/stima_tucil1_23045/Main.java
@@ -1,7 +1,7 @@
 package io.github.nadhifradityo.stima_tucil1_23045;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+public class Main {
+	public static void main(String[] args) {
+		
+	}
 }

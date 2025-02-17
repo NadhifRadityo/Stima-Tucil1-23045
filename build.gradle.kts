8445c0ab0d56a986fa575ec930ec90117d441efb
Nadhif Radityo <nadhifradityo@gmail.com>
2025-02-17 22:29:01 +0700

    feat: restructure project directories

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+	application
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("io.github.spair:imgui-java-app:1.89.0")
+}
+
+sourceSets {
+	main {
+		java {
+			srcDir("src/java")
+		}
+		resources {
+			srcDir("src/resources")
+		}
+	}
+	test {
+		java {
+			srcDir("test/java")
+		}
+		resources {
+			srcDir("test/resources")
+		}
+	}
+}
+
+testing {
+	suites {
+		val test by getting(JvmTestSuite::class) {
+			useJUnitJupiter("5.11.4")
+		}
+	}
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+application {
+	mainClass.set("io.github.nadhifradityo.stima_tucil1_23045.Main")
+}
+tasks.jar {
+	manifest {
+		attributes["Main-Class"] = "io.github.nadhifradityo.stima_tucil1_23045.Main"
+	}
+	from(configurations.runtimeClasspath.get().map { zipTree(it) })
+	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
---
+++ src/java/io/github/nadhifradityo/stima_tucil1_23045/Main.java
@@ -0,0 +1,7 @@
+package io.github.nadhifradityo.stima_tucil1_23045;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
